209ddc749588355fbd15e6f0c3432f35f4bef99d
yzp0n <yzp0n@coe.ad.jp>
2023-03-28 01:24:24 +0000

    protoc/grpc deps update

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.30
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.3
