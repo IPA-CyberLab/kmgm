d47f8fdc9428727fcc2d687b7fef51fec2e73a51
yzp0n <yzp0n@coe.ad.jp>
2023-10-28 04:11:11 +0000

    migrate to buf

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-set -euo pipefail
+PREFIX="/usr/local" && \
+VERSION="1.27.2" && \
+curl -sSL \
+"https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-$(uname -s)-$(uname -m).tar.gz" | \
+sudo tar -xvzf - -C "${PREFIX}" --strip-components 1
 
-go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.30
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.3
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
---
+++ gen.go
@@ -0,0 +1,2 @@
+//go:generate buf generate
+package pb
