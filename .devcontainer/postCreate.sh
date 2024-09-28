e9e93b182410f45ee2bb24c4c0faae091f1e6691
yzp0n <yzp0n@coe.ad.jp>
2024-09-28 11:23:16 +0000

    update .devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 PREFIX="/usr/local" && \
-VERSION="1.27.2" && \
+VERSION="1.42.0" && \
 curl -sSL \
 "https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-$(uname -s)-$(uname -m).tar.gz" | \
 sudo tar -xvzf - -C "${PREFIX}" --strip-components 1
-
-go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
