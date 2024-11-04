8e3bba5a53f3815b6ace4c82f6efc4d3f57ba6f3
yzp0n <yzp0n@coe.ad.jp>
2024-11-04 13:49:02 +0000

    actually add -c shorthand

--- cmd/kmgm/app/appflags/appflags.go
+++ cmd/kmgm/app/appflags/appflags.go
@@ -3,7 +3,7 @@ package appflags
 type AppFlags struct {
 	BaseDir        string `yaml:"baseDir" flags:"basedir,The root directory storing all kmgm data,"`
 	Profile        string `yaml:"profile" flags:"profile,Name of the profile to operate against"`
-	Config         string `flags:"config,Read the specified YAML config file instead of interactive prompt.,,path"`
+	Config         string `flags:"config,Read the specified YAML config file instead of interactive prompt.,c,path"`
 	LogJson        bool   `flags:"log-json,Format logs in json"`
 	NoGeoIp        bool   `flags:"no-geo-ip,Disable querying ip-api.com for geolocation data"`
 	LogLocation    bool   `flags:"log-location,Annotate logs with code location where the log was output"`
